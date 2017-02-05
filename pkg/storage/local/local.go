fba287f16260ae2f5c3a59491d449ca359160542
uruddarraju <rudaykumarraju@gmail.com>
2017-02-05 14:08:02 -0800

    Adding List function to local storage implementation

--- pkg/storage/local/local.go
+++ pkg/storage/local/local.go
@@ -4,30 +4,71 @@ import (
 	"fmt"
 
 	"github.com/uruddarraju/thyra/pkg/runtime"
-	"golang.org/x/net/context"
 	"github.com/uruddarraju/thyra/pkg/storage"
+	"golang.org/x/net/context"
 )
 
 type LocalStorage struct {
 	store map[string]map[string]runtime.Object
 }
 
-func (ls *LocalStorage) List(ctx context.Context, key string, listObj runtime.Object) error {
-	objType := listObj.GetKind()
-	typeFound, typeStore := ls.store[objType]
+func (ls *LocalStorage) List(ctx context.Context, options ListOptions) ([]runtime.Object, error) {
+	objType := options.Type
+	typeStore, typeFound := ls.store[objType]
 	if !typeFound {
-		return storage.NewNotFoundError(fmt.Sprintf("Unable to find type %s", objType))
+		return nil, storage.NewNotFoundError(fmt.Sprintf("Unable to find type %s, check if it already registered to storage", objType))
 	}
 
-	typeStore[key]
-	return nil, nil
+	// The storage should also make sure that all entries have unique name for a given object type
+	// So, a name filter would trump any other filter provided in the options
+	if len(options.Name) > 0 {
+		result, exists := typeStore[options.Name]
+		if !exists {
+			return nil, storage.NewNotFoundError(fmt.Sprintf("Object not found"))
+		}
+		return result, nil
+	}
+
+	// Very inefficient to do Lists as we are iterating on maps, but for local, this should be ok.
+	result := make([]runtime.Object, 0)
+	for _, v := range typeStore {
+		if ls.filter(v, options.LabelSelector) {
+			append(result, v)
+		}
+	}
+
+	return result
+}
+
+// Each keyname provided in the filter should exist in the object metadata with values matching.
+// Object metadata can have more key-value pairs than the selector.
+func (ls *LocalStorage) filter(object runtime.Object, selector map[string]string) bool {
+	for key, value := range selector {
+		if object.GetMetadata() == nil {
+			return false
+		}
+		if objectValue, exists := object.GetMetadata()[key]; exists && value == objectValue {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+// Defined in interface, remove this after you finished implementing the local storage
+type ListOptions struct {
+	Name          string
+	Type          string
+	LabelSelector map[string]string
 }
 
 type Storage interface {
-	Find(ctx context.Context, lookup runtime.Object) ([]runtime.Object, error)
+	List(ctx context.Context, options ListOptions) ([]runtime.Object, error)
+	Get(ctx context.Context, lookup runtime.Object) (runtime.Object, error)
 	Create(ctx context.Context, item runtime.Object) error
-	Update(ctx context.Context, item runtime.Object, original runtime.Object) error
+	Update(ctx context.Context, item runtime.Object, original runtime.Object) (runtime.Object, error)
 	Delete(ctx context.Context, item runtime.Object) error
-	Clear(ctx context.Context, lookup runtime.Object) (int, error)
 }
 
+// Until here
