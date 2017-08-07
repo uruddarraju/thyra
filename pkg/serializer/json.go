af3c517ea4049c13c054da3b5c49e127d546be5a
uruddarraju <rudaykumarraju@gmail.com>
2017-08-07 01:09:19 -0700

    Adding json serializer

--- pkg/serializer/json.go
+++ pkg/serializer/json.go
@@ -5,15 +5,14 @@ import (
 	"net/http"
 
 	"github.com/uruddarraju/thyra/pkg/api/runtime"
-	"github.com/uruddarraju/thyra/pkg/apis/thyra/v1"
 )
 
-func Decode(req *http.Request) (runtime.Object, error) {
+func Decode(req *http.Request, into runtime.Object) (runtime.Object, error) {
 	decoder := json.NewDecoder(req.Body)
-	var meta v1.ObjectMeta
-	err := decoder.Decode(meta)
+	err := decoder.Decode(into)
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+
+	return into, nil
 }
